99cd7dc1ebac8c240daeec9b628912b341f62f1b
Davis Goodin <dagood@microsoft.com>
2021-03-08 15:47:45 +0000

    Merged PR 13424: Fix diffs between official tar.gz and ours
    Fix diffs between official tar.gz and ours
    
    Adds more filtering to pack.sh to remove diff between the file list in our tarball vs. the official one. Tested by applying changes onto the go1.16 tag and comparing against the official 1.16 linux-amd64 binary release.
    
    Introduces '--test' and '--pack' in build.sh for more convenient and faster (if skipping tests) local dev flows. The '--test' arg could also be used later to speed up rolling vs. PR validation builds, which currently both run tests.

--- microsoft/build.sh
+++ microsoft/build.sh
@@ -18,6 +18,59 @@ done
 
 scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
 
+build=1
+test=
+pack=
+
+# Print usage information and exit 0 if no error message is provided.
+# $1: An error message to display. If provided, this function will exit 1.
+usage() {
+  exit_code=0
+
+  if [ "${1:-}" ]; then
+    echo "Error: $1"
+    echo ""
+    exit_code=1
+  fi
+
+  echo "$0 builds Go, optionally running tests and packing a tar.gz file."
+  echo ""
+  echo "This script is used by CI for PR validation and building rolling builds, and can be used to reproduce issues in those environments. It downloads and installs a local copy of Go to ensure a consistent version."
+  echo ""
+  echo "Options:"
+  echo "  --skip-build  Disable building Go."
+  echo "  --test        Enable running tests."
+  echo "  --pack        Enable creating a tar.gz file similar to the official Go binary release."
+  echo "  -h|--help     Print this help message and exit."
+  echo ""
+  echo "Example: Perform a build, run tests on it, and produce a tar.gz file:"
+  echo "  $0 --test --pack"
+
+  exit "$exit_code"
+}
+
+while [[ $# > 0 ]]; do
+  case "$1" in
+    --skip-build)
+      build=
+      ;;
+    --test)
+      test=1
+      ;;
+    --pack)
+      pack=1
+      ;;
+    -h|--help)
+      usage
+      ;;
+    *)
+      usage "Unexpected argument: $1"
+      ;;
+  esac
+
+  shift
+done
+
 # We need Go installed in order to build Go, but our common build environment
 # doesn't have it pre-installed. This CI script installs a consistent, official
 # version of Go to a hidden directory in $HOME to handle this. This also makes
@@ -66,5 +119,23 @@ fi
 (
   cd "$scriptroot/../src"
   PATH="$PATH:$stage0_dir/go/bin"
-  ./all.bash
+
+  if [ "$build" ]; then
+    echo "Running main build..."
+    ./make.bash
+
+    # Build race detection runtime. It's included with the official Go binary distribution.
+    echo "Building race runtime..."
+    ../bin/go install -race -a std
+  fi
+
+  if [ "$test" ]; then
+    echo "Running tests..."
+    ./run.bash --no-rebuild
+  fi
+
+  if [ "$pack" ]; then
+    echo "Running pack..."
+    "$scriptroot/pack.sh"
+  fi
 )
--- microsoft/pack.sh
+++ microsoft/pack.sh
@@ -19,21 +19,56 @@ done
 scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
 
 artifacts_dir="$scriptroot/artifacts"
-mkdir "$artifacts_dir" || :
+test ! -d "$artifacts_dir" && mkdir "$artifacts_dir"
 
 version=${BUILD_BUILDNUMBER:-dev}
-go_tarball="$artifacts_dir/go.$version.tar.gz"
 
 (
   cd "$scriptroot/.."
 
-  set -x
+  if [ ! -f bin/go ]; then
+    echo "Error: 'bin/go' not found! Build must be complete to pack it. Use build.sh instead."
+    exit 1
+  fi
 
+  GOOS=$(bin/go env GOOS)
+  GOARCH=$(bin/go env GOARCH)
+  echo "Detected bin/go built for GOOS=$GOOS, GOARCH=$GOARCH"
+
+  go_tarball="$artifacts_dir/go.$version.$GOOS-$GOARCH.tar.gz"
+  go_tarball_excludes="$artifacts_dir/exclude-from-targz.txt"
+
+  echo "Creating $go_tarball ..."
+
+  # Release branches have a 'VERSION' file, others may not. Include if exists.
+  test -f VERSION && version_file=VERSION
+
+  # The official Go tar.gz binary distribution is a tar.gz of the Go directory, with a bunch of
+  # files removed. Here, match that behavior by creating an exclusion file and passing to 'tar'.
+
+  # Begin the file by excluding general build intermediates.
+  echo "pkg/obj" > "$go_tarball_excludes"
+
+  # Exclude the compiler build.
+  echo "pkg/${GOOS}_${GOARCH}/cmd" >> "$go_tarball_excludes"
+
+  # Exclude the api checker prebuilt binary.
+  echo "pkg/tool/${GOOS}_${GOARCH}/api" >> "$go_tarball_excludes"
+
+  # Exclude prebuilt race detector syso file for all but the current OS/ARCH.
+  find src/runtime/race -iname 'race_*.syso' -and ! -iname "race_${GOOS}_${GOARCH}.syso" \
+    >> "$go_tarball_excludes"
+
+  # Create the tarball. Pass in the exclusion list, and also be selective in which files/dirs are
+  # given as input.
   tar -cz \
     --numeric-owner \
     --transform 's,^,go/,' \
-    --exclude=pkg/obj \
+    --exclude-from="$go_tarball_excludes" \
     *.txt *.ico *.md AUTHORS CONTRIBUTORS LICENSE PATENTS \
     api bin doc lib misc pkg src test \
+    ${version_file:-} \
     -f "$go_tarball"
+
+  echo "Done!"
 )
