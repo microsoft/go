e3168d7a373d76def7d16da7a9359571ecf24f2e
Davis Goodin <dagood@users.noreply.github.com>
2021-03-16 15:57:23 -0500

    Update infra file license header to match upstream (#20)

--- microsoft/mirror-upstream-refs.sh
+++ microsoft/mirror-upstream-refs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
 
 set -euo pipefail
 
