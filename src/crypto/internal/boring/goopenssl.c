82af377dabc10d21dcf2179e4a857247695f80e3
qmuntal <qmuntaldiaz@microsoft.com>
2021-12-01 09:04:58 +0100

    convert indentation to spaces

--- src/crypto/internal/boring/goopenssl.c
+++ src/crypto/internal/boring/goopenssl.c
@@ -26,27 +26,27 @@ _goboringcrypto_load_openssl_functions()
 
 #define DEFINEFUNC(ret, func, args, argscall) \
     _g_##func = dlsym(handle, #func);         \
-	if (_g_##func == NULL) { fprintf(stderr, "Cannot get required symbol " #func " from libcrypto\n"); abort(); }
+    if (_g_##func == NULL) { fprintf(stderr, "Cannot get required symbol " #func " from libcrypto\n"); abort(); }
 #define DEFINEFUNCINTERNAL(ret, func, args, argscall) \
     _g_internal_##func = dlsym(handle, #func);        \
-	if (_g_internal_##func == NULL) { fprintf(stderr, "Cannot get required symbol " #func " from libcrypto\n"); abort(); }
+    if (_g_internal_##func == NULL) { fprintf(stderr, "Cannot get required symbol " #func " from libcrypto\n"); abort(); }
 #define DEFINEFUNC_LEGACY(ret, func, args, argscall)  \
     if (v1_0_sentinel != NULL)                        \
-	{                                                 \
-		DEFINEFUNCINTERNAL(ret, func, args, argscall) \
-	}
+    {                                                 \
+        DEFINEFUNCINTERNAL(ret, func, args, argscall) \
+    }
 #define DEFINEFUNC_110(ret, func, args, argscall)     \
     if (v1_0_sentinel == NULL)                        \
-	{                                                 \
-		DEFINEFUNCINTERNAL(ret, func, args, argscall) \
-	}
+    {                                                 \
+        DEFINEFUNCINTERNAL(ret, func, args, argscall) \
+    }
 #define DEFINEFUNC_RENAMED(ret, func, oldfunc, args, argscall)                                                               \
     tmp_ptr = dlsym(handle, #func);                                                                                          \
-	if (tmp_ptr == NULL)                                                                                                     \
-	{                                                                                                                        \
+    if (tmp_ptr == NULL)                                                                                                     \
+    {                                                                                                                        \
         tmp_ptr = dlsym(handle, #oldfunc);                                                                                   \
-	    if (tmp_ptr == NULL) { fprintf(stderr, "Cannot get required symbol " #oldfunc " from libcrypto\n"); abort(); }       \
-	}                                                                                                                        \
+        if (tmp_ptr == NULL) { fprintf(stderr, "Cannot get required symbol " #oldfunc " from libcrypto\n"); abort(); }       \
+    }                                                                                                                        \
     _g_internal_##func = tmp_ptr;
 
 FOR_ALL_OPENSSL_FUNCTIONS
@@ -61,73 +61,73 @@ FOR_ALL_OPENSSL_FUNCTIONS
 static void
 _goboringcrypto_DLOPEN(const char* libraryName)
 {
-	handle = dlopen(libraryName, RTLD_NOW | RTLD_GLOBAL);
+    handle = dlopen(libraryName, RTLD_NOW | RTLD_GLOBAL);
 }
 
 void*
 _goboringcrypto_internal_DLOPEN_OPENSSL(void)
 {
-	if (handle)
-	{
-		return handle;
-	}
+    if (handle)
+    {
+        return handle;
+    }
 
-	// If there is an override of the version specified using the GO_OPENSSL_VERSION_OVERRIDE
-	// env variable, try to load that first.
-	// The format of the value in the env variable is expected to be the version numbers,
-	// like 1.0.0, 1.0.2 etc.
-	char* versionOverride = getenv("GO_OPENSSL_VERSION_OVERRIDE");
-	if ((versionOverride != NULL) && strnlen(versionOverride, MaxVersionStringLength + 1) <= MaxVersionStringLength)
-	{
-		char soName[sizeof(SONAME_BASE) + MaxVersionStringLength] = SONAME_BASE;
-		strcat(soName, versionOverride);
-		_goboringcrypto_DLOPEN(soName);
-	}
+    // If there is an override of the version specified using the GO_OPENSSL_VERSION_OVERRIDE
+    // env variable, try to load that first.
+    // The format of the value in the env variable is expected to be the version numbers,
+    // like 1.0.0, 1.0.2 etc.
+    char* versionOverride = getenv("GO_OPENSSL_VERSION_OVERRIDE");
+    if ((versionOverride != NULL) && strnlen(versionOverride, MaxVersionStringLength + 1) <= MaxVersionStringLength)
+    {
+        char soName[sizeof(SONAME_BASE) + MaxVersionStringLength] = SONAME_BASE;
+        strcat(soName, versionOverride);
+        _goboringcrypto_DLOPEN(soName);
+    }
 
 #if OPENSSL_VERSION_NUMBER >= OPENSSL_VERSION_3_0_RTM
-	if (handle == NULL)
-	{
-		_goboringcrypto_DLOPEN(MAKELIB("3"));
-	}
+    if (handle == NULL)
+    {
+        _goboringcrypto_DLOPEN(MAKELIB("3"));
+    }
 #elif OPENSSL_VERSION_NUMBER >= OPENSSL_VERSION_1_1_0_RTM
-	if (handle == NULL)
-	{
-		_goboringcrypto_DLOPEN(MAKELIB("1.1"));
-	}
+    if (handle == NULL)
+    {
+        _goboringcrypto_DLOPEN(MAKELIB("1.1"));
+    }
 
-	// FreeBSD uses a different suffix numbering convention.
-	// Current supported FreeBSD releases should use the order .11 -> .111
-	if (handle == NULL)
-	{
-		_goboringcrypto_DLOPEN(MAKELIB("11"));
-	}
+    // FreeBSD uses a different suffix numbering convention.
+    // Current supported FreeBSD releases should use the order .11 -> .111
+    if (handle == NULL)
+    {
+        _goboringcrypto_DLOPEN(MAKELIB("11"));
+    }
 
-	if (handle == NULL)
-	{
-		_goboringcrypto_DLOPEN(MAKELIB("111"));
-	}
+    if (handle == NULL)
+    {
+        _goboringcrypto_DLOPEN(MAKELIB("111"));
+    }
 #else
-	if (handle == NULL)
-	{
-		// Debian 9 has dropped support for SSLv3 and so they have bumped their soname. Let's try it
-		// before trying the version 1.0.0 to make it less probable that some of our other dependencies
-		// end up loading conflicting version of libcrypto.
-		_goboringcrypto_DLOPEN(MAKELIB("1.0.2"));
-	}
+    if (handle == NULL)
+    {
+        // Debian 9 has dropped support for SSLv3 and so they have bumped their soname. Let's try it
+        // before trying the version 1.0.0 to make it less probable that some of our other dependencies
+        // end up loading conflicting version of libcrypto.
+        _goboringcrypto_DLOPEN(MAKELIB("1.0.2"));
+    }
 
-	if (handle == NULL)
-	{
-		// Now try the default versioned so naming as described in the OpenSSL doc
-		_goboringcrypto_DLOPEN(MAKELIB("1.0.0"));
-	}
+    if (handle == NULL)
+    {
+        // Now try the default versioned so naming as described in the OpenSSL doc
+        _goboringcrypto_DLOPEN(MAKELIB("1.0.0"));
+    }
 
-	if (handle == NULL)
-	{
-		// Fedora derived distros use different naming for the version 1.0.0
-		_goboringcrypto_DLOPEN(MAKELIB("10"));
-	}
+    if (handle == NULL)
+    {
+        // Fedora derived distros use different naming for the version 1.0.0
+        _goboringcrypto_DLOPEN(MAKELIB("10"));
+    }
 #endif
-	return handle;
+    return handle;
 }
 
 #if OPENSSL_VERSION_NUMBER < OPENSSL_VERSION_1_1_0_RTM
@@ -138,31 +138,31 @@ int
 _goboringcrypto_internal_OPENSSL_setup(void) 
 {
     _goboringcrypto_load_openssl_functions();
-	// OPENSSL_init initialize FIPS callbacks and rand generator.
-	// no-op from OpenSSL 1.1.1 onwards.
+    // OPENSSL_init initialize FIPS callbacks and rand generator.
+    // no-op from OpenSSL 1.1.1 onwards.
 #if OPENSSL_VERSION_NUMBER >= OPENSSL_VERSION_1_0_2_RTM
-	_goboringcrypto_internal_OPENSSL_init();
+    _goboringcrypto_internal_OPENSSL_init();
 #endif
 #if OPENSSL_VERSION_NUMBER < OPENSSL_VERSION_1_1_0_RTM
-	if (_goboringcrypto_OPENSSL_thread_setup() != 1)
-	{
-		return 0;
-	}
-	// Load all algorithms and the openssl configuration file.
-	_goboringcrypto_internal_OPENSSL_add_all_algorithms_conf();
+    if (_goboringcrypto_OPENSSL_thread_setup() != 1)
+    {
+        return 0;
+    }
+    // Load all algorithms and the openssl configuration file.
+    _goboringcrypto_internal_OPENSSL_add_all_algorithms_conf();
 
-	// Ensure that the error message table is loaded.
-	_goboringcrypto_internal_ERR_load_crypto_strings();
-	return 1;
+    // Ensure that the error message table is loaded.
+    _goboringcrypto_internal_ERR_load_crypto_strings();
+    return 1;
 
 #else
-	// In OpenSSL 1.0 we call OPENSSL_add_all_algorithms_conf() and ERR_load_crypto_strings(),
-	// so do the same for 1.1
-	return _goboringcrypto_internal_OPENSSL_init_crypto(
-			OPENSSL_INIT_ADD_ALL_CIPHERS |
-			OPENSSL_INIT_ADD_ALL_DIGESTS |
-			OPENSSL_INIT_LOAD_CONFIG |
-			OPENSSL_INIT_LOAD_CRYPTO_STRINGS,
-			NULL);
+    // In OpenSSL 1.0 we call OPENSSL_add_all_algorithms_conf() and ERR_load_crypto_strings(),
+    // so do the same for 1.1
+    return _goboringcrypto_internal_OPENSSL_init_crypto(
+            OPENSSL_INIT_ADD_ALL_CIPHERS |
+            OPENSSL_INIT_ADD_ALL_DIGESTS |
+            OPENSSL_INIT_LOAD_CONFIG |
+            OPENSSL_INIT_LOAD_CRYPTO_STRINGS,
+            NULL);
 #endif
 }
