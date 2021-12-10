3cb379f8724116b9de894f0f538d345245dc72a3
qmuntal <qmuntaldiaz@microsoft.com>
2021-12-10 15:31:38 +0100

    reenable std gcm tests

--- src/crypto/cipher/gcm_test.go
+++ src/crypto/cipher/gcm_test.go
@@ -8,7 +8,6 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/internal/boring"
 	"crypto/rand"
 	"encoding/hex"
 	"errors"
@@ -227,10 +226,7 @@ var aesGCMTests = []struct {
 	},
 }
 
-const gcmStandardNonceSize = 12
-
 func TestAESGCM(t *testing.T) {
-outer:
 	for i, test := range aesGCMTests {
 		key, _ := hex.DecodeString(test.key)
 		aes, err := aes.NewCipher(key)
@@ -239,12 +235,6 @@ outer:
 		}
 
 		nonce, _ := hex.DecodeString(test.nonce)
-		if boring.Enabled() {
-			keyLen := len(key) * 8
-			if keyLen != 128 && keyLen != 256 {
-				continue
-			}
-		}
 		plaintext, _ := hex.DecodeString(test.plaintext)
 		ad, _ := hex.DecodeString(test.ad)
 		tagSize := (len(test.result) - len(test.plaintext)) / 2
@@ -253,9 +243,6 @@ outer:
 		switch {
 		// Handle non-standard tag sizes
 		case tagSize != 16:
-			if boring.Enabled() {
-				break outer
-			}
 			aesgcm, err = cipher.NewGCMWithTagSize(aes, tagSize)
 			if err != nil {
 				t.Fatal(err)
@@ -271,9 +258,6 @@ outer:
 
 		// Handle non-standard nonce sizes
 		case len(nonce) != 12:
-			if boring.Enabled() {
-				break outer
-			}
 			aesgcm, err = cipher.NewGCMWithNonceSize(aes, len(nonce))
 			if err != nil {
 				t.Fatal(err)
@@ -373,9 +357,6 @@ func TestTagFailureOverwrite(t *testing.T) {
 }
 
 func TestGCMCounterWrap(t *testing.T) {
-	if boring.Enabled() {
-		t.Skip("skipping GCM counter test in boring mode")
-	}
 	// Test that the last 32-bits of the counter wrap correctly.
 	tests := []struct {
 		nonce, tag string
@@ -428,9 +409,6 @@ func wrap(b cipher.Block) cipher.Block {
 }
 
 func TestGCMAsm(t *testing.T) {
-	if boring.Enabled() {
-		t.Skip("skipping test in boring mode")
-	}
 	// Create a new pair of AEADs, one using the assembly implementation
 	// and one using the generic Go implementation.
 	newAESGCM := func(key []byte) (asm, generic cipher.AEAD, err error) {
