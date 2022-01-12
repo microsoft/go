e1585d2c5f4d8d665273373d646802898347dd95
qmuntal <qmuntaldiaz@microsoft.com>
2022-01-12 12:14:44 +0100

    remove init function and define Init, FIPS, SetFIPS

--- src/crypto/internal/backend/internal/openssl/hmac_test.go
+++ src/crypto/internal/backend/internal/openssl/hmac_test.go
@@ -15,9 +15,6 @@ import (
 // Previously would cause panic because of incorrect
 // stack allocation of opaque OpenSSL type.
 func TestNewHMAC(t *testing.T) {
-	if !Enabled() {
-		t.Skip("boringcrypto: skipping test, FIPS not enabled")
-	}
 	mac := NewHMAC(NewSHA256, nil)
 	mac.Write([]byte("foo"))
 	t.Logf("%x\n", mac.Sum(nil))
