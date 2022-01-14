58ca916333169f298032f39442d94dcbf7cbc28b
qmuntal <qmuntaldiaz@microsoft.com>
2022-01-14 15:31:50 +0100

    openssl should use internal linking

--- src/cmd/link/internal/ld/lib.go
+++ src/cmd/link/internal/ld/lib.go
@@ -1013,7 +1013,7 @@ var hostobj []Hostobj
 // These packages can use internal linking mode.
 // Others trigger external mode.
 var internalpkg = []string{
-	"crypto/internal/boring",
+	"crypto/internal/backend/internal/openssl",
 	"crypto/x509",
 	"net",
 	"os/user",
