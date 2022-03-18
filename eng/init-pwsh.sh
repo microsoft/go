52f84ccf8692b8bc141a79a4e8e19668c67e9428
Davis Goodin <dagood@users.noreply.github.com>
2022-03-18 12:56:29 -0500

    Add arm64 internal builds and build script support (#474)

--- eng/init-pwsh.sh
+++ eng/init-pwsh.sh
@@ -9,9 +9,29 @@
 
 set -euo pipefail
 
-pwsh_version='7.1.3'
-pwsh_sha256='9f853fb8f7c7719005bd1054fa13ca4d925c519b893f439dd2574e84503e6a85'
-pwsh_url="https://github.com/PowerShell/PowerShell/releases/download/v$pwsh_version/powershell-$pwsh_version-linux-x64.tar.gz"
+# Default values for x64.
+pwsh_version='7.2.1'
+pwsh_sha256='337d9864799ad09b46d261071b9f835f69f078814409bc2681f4cc2857b6bda5'
+pwsh_arch='x64'
+
+# 'uname' approach adapted from .NET install script: https://github.com/dotnet/install-scripts/blob/df8f863720a462448ad244f03ffeb619f0631bad/src/dotnet-install.sh#L295-L315
+if command -v uname > /dev/null; then
+  CPUName=$(uname -m)
+  case $CPUName in
+    armv*l)
+      echo "armv*l was detected, but it is not supported by the microsoft/go build infrastructure."
+      exit 1
+      ;;
+    aarch64|arm64)
+      pwsh_sha256='f0d6c9c36d69e1466e5a9412085ef52cafd10b73f862d29479b806279a2975f4'
+      pwsh_arch='arm64'
+      ;;
+  esac
+else
+  echo "uname command not detected. Assuming $pwsh_arch."
+fi
+
+pwsh_url="https://github.com/PowerShell/PowerShell/releases/download/v$pwsh_version/powershell-$pwsh_version-linux-$pwsh_arch.tar.gz"
 
 # pwsh must be installed outside of the Go repo. If it's in the repo, longtest "TestAllDependencies"
 # fails. It tries to traverse the pwsh directory and can't handle the "no such file or directory"
@@ -22,6 +42,7 @@ download_complete_indicator="$pwsh_dir/.downloaded"
 
 if [ ! -f "$download_complete_indicator" ]; then
   echo "Downloading PowerShell $pwsh_version and extracting to '$pwsh_dir' ..."
+  echo "URL: $pwsh_url"
 
   # Clear existing dir in case it's in a broken state.
   rm -rf "$pwsh_dir"
