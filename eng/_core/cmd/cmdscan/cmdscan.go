26aadf8895949edad8f4285789c812a4b4c9ab9e
Davis Goodin <dagood@users.noreply.github.com>
2022-08-11 15:03:17 -0500

    Use env for cmdscan rules (#691)
    (cherry picked from commit 9105cc64d4b138bea0ecba487cf88b943dd0bec4)

--- eng/_core/cmd/cmdscan/cmdscan.go
+++ eng/_core/cmd/cmdscan/cmdscan.go
@@ -1,52 +1,131 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-// Cmdscan runs the command given to it in os.Args and monitors the output for text patterns that
-// should be elevated to AzDO Pipeline warnings using AzDO logging commands. Timeline events can be
-// discovered more easily in the UI and by automated tools like runfo.
-//
-// If the pattern is associated with a known issue, the warning includes a link.
-//
-// Uses the "log issue" pipeline logging command: https://docs.microsoft.com/en-us/azure/devops/pipelines/scripts/logging-commands?view=azure-devops&tabs=bash#logissue-log-an-error-or-warning
 package main
 
 import (
 	"bufio"
+	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"os/exec"
 	"regexp"
+	"strings"
 )
 
-var filters = []filter{
-	{
-		"access denied",
-		"https://github.com/microsoft/go/issues/241",
-		regexp.MustCompile("(?i)Access is denied"),
-	},
+const description = `
+Cmdscan runs the command given to it as args and monitors the output for text patterns that should
+be elevated to AzDO Pipeline warnings using AzDO logging commands. Timeline events can be discovered
+more easily in the UI and by automated tools like runfo.
+
+Uses the "log issue" pipeline logging command to create timeline warnings:
+https://docs.microsoft.com/en-us/azure/devops/pipelines/scripts/logging-commands?view=azure-devops&tabs=bash#logissue-log-an-error-or-warning
+
+To specify the rules to match, pass something like "GO_CMDSCAN_RULE_" to envprefix. This detects any
+env variables that start with that string and interprets their values as JSON specifying a rule. The
+part of the env var after the prefix is what the rule should be called in logs. If parsing is not
+successful, that rule is ignored. Cmdscan writes logs about what it finds.
+
+When using an AzDO pipeline, SHOUT_CASE is recommended so AzDO's env var naming conversion doesn't
+change the result:
+https://docs.microsoft.com/en-us/azure/devops/pipelines/process/variables?view=azure-devops&tabs=yaml%2Cbatch#environment-variables
+
+Use "--" to unambiguously separate the flag with the command to run.
+
+The format for a rule is a JSON object with regex string "pattern" and optionally a "url" string
+that contains more information about the issue. For example:
+
+  {"pattern": "(?i)Access is denied", "url": "https://github.com/microsoft/go/issues/241"}
+
+Example cmdscan call:
+
+  pwsh eng/run.ps1 cmdscan -envprefix GoCmdscanRule -- pwsh eng/run.ps1 build -test
+`
+
+type rule struct {
+	Pattern string `json:"pattern"`
+	URL     string `json:"url"`
 }
 
 type filter struct {
 	// name is a simple description of the detected error that should be uniquely searchable so
 	// runfo can keep track of this issue with "contains" searches on each timeline element.
 	name string
-	// trackingIssue is an optional URL that links to more information about this error.
-	trackingIssue string
+	// url is an optional URL that links to more information about this error.
+	url string
 	// regexp is the regex that is matched against each line of output to find an issue.
 	regexp *regexp.Regexp
 }
 
+var filters []filter
+
 func main() {
+	help := flag.Bool("h", false, "Print this help message.")
+	prefix := flag.String("envprefix", "", "The env var prefix to use to find scan rules.")
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage:\n")
+		flag.PrintDefaults()
+		fmt.Fprintf(flag.CommandLine.Output(), "%s\n", description)
+	}
+
+	flag.Parse()
+	if *help {
+		flag.Usage()
+		return
+	}
+
+	if *prefix != "" {
+		log.Printf("Searching for rules with env var prefix %v...\n", *prefix)
+		for _, e := range os.Environ() {
+			if envName, envValue, ok := strings.Cut(e, "="); ok {
+				if before, ruleName, ok := strings.Cut(envName, *prefix); ok && before == "" && ruleName != "" {
+					f, err := parseFilter(ruleName, envValue)
+					if err != nil {
+						log.Printf("Failed to parse rule %q: %v\n", envName, err)
+						continue
+					}
+					log.Printf("Parsed rule %q: %q (%v)\n", envName, f.regexp.String(), f.url)
+					filters = append(filters, *f)
+				}
+			}
+		}
+		log.Printf("Found %v rules defined by env vars.\n", len(filters))
+	}
+
 	if err := run(); err != nil {
 		log.Fatalln(err)
 	}
 }
 
+func parseFilter(name, envValue string) (*filter, error) {
+	var r rule
+
+	if err := json.Unmarshal([]byte(envValue), &r); err != nil {
+		return nil, err
+	}
+	if r.Pattern == "" {
+		return nil, errors.New("rule defines no pattern")
+	}
+
+	exp, err := regexp.Compile(r.Pattern)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse rule regex: %v", err)
+	}
+
+	return &filter{
+		name:   name,
+		url:    r.URL,
+		regexp: exp,
+	}, nil
+}
+
 func run() error {
-	cmd := exec.Command(os.Args[1], os.Args[2:]...)
-	log.Printf("Detected %v scan patterns defined in eng/_core/cmd/cmdscan/cmdscan.go\n", len(filters))
+	cmd := exec.Command(flag.Args()[0], flag.Args()[1:]...)
 	log.Printf("Running: %v\n", cmd)
 
 	outPipe, err := cmd.StdoutPipe()
@@ -93,8 +172,8 @@ func scan(r io.Reader, commands, echo *os.File) error {
 
 func warn(f *filter, line string) string {
 	var issueLink string
-	if f.trackingIssue != "" {
-		issueLink = " (" + f.trackingIssue + ")"
+	if f.url != "" {
+		issueLink = " (" + f.url + ")"
 	}
 
 	return fmt.Sprintf("##vso[task.logissue type=warning]%q%v: %v\n", f.name, issueLink, line)
