140c9942212389ad5d803ab5ef37d18d880d85ba
Quim Muntal <qmuntaldiaz@microsoft.com>
2025-12-04 12:32:04 +0000

    fix lint

--- eng/_util/cmd/upgrader/main.go
+++ eng/_util/cmd/upgrader/main.go
@@ -146,6 +146,7 @@ func upgradeDependency(info *depsInfo) error {
 }
 
 var goBinary = sync.OnceValue(func() string {
+	//lint:ignore SA1019 we want to know the binary that built us
 	path := filepath.Join(runtime.GOROOT(), "bin", "go")
 	if runtime.GOOS == "windows" {
 		path += ".exe"
